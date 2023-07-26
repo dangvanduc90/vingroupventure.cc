186f2e5dd549b48615accd50bb0bee3c61fac534
ducdv <ducdv@dlsinc.com>
2023-07-27 00:29:12 +0700

    update base api url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BACKEND_API_URL: '"https://api.vingroupventure.cc"',
+  BACKEND_API_URL: '"https://api.vingroupventure.cc/api"',
   VINGROUP_API_URL: '"http://api.vingroupventures.cc/api"',
 }
